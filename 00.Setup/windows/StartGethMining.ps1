9108cb2c016878b67d5abe2e18855a29fbed0bdf
Eric Maino <ericmai@microsoft.com>
2017-02-09 22:15:37 -0800

    Update geth to download and set path for solc

--- 00.Setup/windows/StartGethMining.ps1
+++ 00.Setup/windows/StartGethMining.ps1
@@ -5,6 +5,16 @@ Set-StrictMode -Version 2
 Import-Module (Join-Path $PSScriptRoot "Common") -Force
 
 $config = [PSCustomObject]@{
+    Name = "Solidity";
+    Uri = "https://github.com/ethereum/solidity/releases/download/v0.4.9/solidity-windows.zip";
+    Version = "0.4.9"
+}
+
+$directory = $config | Invoke-Unpack
+$SolcPath = (Get-ChildItem -Recurse -Path $directory "solc.exe" | Select -First 1).Directory
+$env:Path += ";$SolcPath"
+
+$config = [PSCustomObject]@{
     Name = "GoEthereum";
     Uri = "https://gethstore.blob.core.windows.net/builds/geth-windows-amd64-1.5.7-da2a22c3.zip";
     Version = "1.5.7"
